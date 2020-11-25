b9ebda6b097c73373cb349c0fc2b0635bf952921
shinyjks <jjebashiny@gmail.com>
2020-11-25 08:59:12 +0530

    Create Main.java
    day1 exercise

---
+++ Eclipse/core-java-exercise-1/src/com/org/Main.java
@@ -0,0 +1,24 @@
+package com.org;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<User> list = new ArrayList<User>();
+		list.add(new User(100, "Shiny", "shiny123"));
+		list.add(new User(102, "Jasmine", "jasmine1234"));
+		list.add(new User(103, "Jasper", "jasper987"));
+		list.add(new User(104, "Amy", "amy456"));
+		list.add(new User(105, "Jerrina", "jerrina123"));
+		
+		for(User user : list) {
+			if(user.getName().length() > 5) {
+				System.out.println("Id = "+user.getId()+", Name = "+user.getName());
+			}
+		}
+	}
+
+}
