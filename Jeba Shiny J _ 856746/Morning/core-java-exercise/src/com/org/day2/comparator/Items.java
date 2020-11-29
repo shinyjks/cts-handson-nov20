ca5a1ce40b426a6eeca6c57e181080445e04c03c
shinyjks <jjebashiny@gmail.com>
2020-11-29 23:12:40 +0530

    Create Items.java

---
+++ Jeba Shiny J _ 856746/Morning/core-java-exercise/src/com/org/day2/comparator/Items.java
@@ -0,0 +1,50 @@
+package com.org.day2.comparator;
+
+public class Items {
+
+	private int id;
+	private String name;
+	private float price;
+
+	public Items() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Items(int id, String name, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Items [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	
+}
