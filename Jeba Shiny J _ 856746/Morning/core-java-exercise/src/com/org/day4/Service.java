70b5ea3fffe6e94f749df73665a535dafaacb0df
shinyjks <jjebashiny@gmail.com>
2020-11-30 11:51:55 +0530

    Create Service.java

---
+++ Jeba Shiny J _ 856746/Morning/core-java-exercise/src/com/org/day4/Service.java
@@ -0,0 +1,45 @@
+package com.org.day4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+
+	List<Account> list = new ArrayList<Account>();
+	
+	public void add(Account a) {
+		list.add(a);
+	}
+	
+	public Account findByAccNo(int accNo) {
+		for(Account a: list) {
+			if(a.getAccNo() == accNo) {
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	public Iterable<Account> display(){
+		return list;
+	}
+	
+	public void remove(int accNo) {
+		for(Account a: list) {
+			if(a.getAccNo() == accNo) {
+				System.out.println(a);
+				list.remove(a);
+			}
+		}
+	}
+	
+	public List<Account> displayByBalance(){
+		List<Account> balance = new ArrayList<>();
+		for(Account a: list) {
+			if(a.getAccBalance() < 1000.0) {
+				balance.add(a);
+			}
+		}
+		return balance;
+	}
+}
