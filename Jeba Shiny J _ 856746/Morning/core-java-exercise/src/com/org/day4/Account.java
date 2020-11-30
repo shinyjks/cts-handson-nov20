b66bf89adc28576c416c4394d8f710ceb55cd575
shinyjks <jjebashiny@gmail.com>
2020-11-30 11:51:43 +0530

    Create Account.java

---
+++ Jeba Shiny J _ 856746/Morning/core-java-exercise/src/com/org/day4/Account.java
@@ -0,0 +1,40 @@
+package com.org.day4;
+
+public class Account {
+
+	private int accNo;
+	private float accBalance;
+	
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Account(int accNo, float accBalance) {
+		super();
+		this.accNo = accNo;
+		this.accBalance = accBalance;
+	}
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+
+	public float getAccBalance() {
+		return accBalance;
+	}
+
+	public void setAccBalance(float accBalance) {
+		this.accBalance = accBalance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [accNo=" + accNo + ", accBalance=" + accBalance + "]";
+	}
+	
+}
